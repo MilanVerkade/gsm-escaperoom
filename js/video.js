e9d2aa84aee113999dcd1516d370f934cbe1cc9e
MilanVerkade <9028442@student.zadkine.nl>
2026-03-05 15:19:28 +0100

    css

---
+++ js/video.js
@@ -0,0 +1,8 @@
+homepage = document.getElementById("container");
+video = document.getElementById("video");
+
+function openVideo() {
+    homepage.style.display = "none";
+    document.getElementById("video").style.display = "block";
+    }
+
