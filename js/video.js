7bdd6270c74f808804c9ef2ff78feb88a51ace48
MilanVerkade <9028442@student.zadkine.nl>
2026-03-06 11:28:52 +0100

    index

--- js/video.js
+++ js/video.js
@@ -1,9 +1,14 @@
 homepage = document.getElementById("container");
 video = document.getElementById("video");
-timer = setInterval()
+
+function timer{
+    timer = setInterval(10000);
+
+}
 
 function openVideo() {
     homepage.style.display = "none";
     document.getElementById("video").style.display = "block";
+
     }
 
