c636363fdfafb415e58cc053443c453da6141bda
MilanVerkade <9028442@student.zadkine.nl>
2026-03-09 15:14:26 +0100

    Update video.js

--- js/video.js
+++ js/video.js
@@ -2,13 +2,9 @@ homepage = document.getElementById("container");
 video = document.getElementById("video");
 video.style.display = "none";
 
-function timer{
-    timer = setInterval(10000);
-}
-
 function openVideo() {
     homepage.style.display = "none";
     document.getElementById("video").style.display = "block";
     alert("Video will start in 10 seconds");
-    }
+}
 
