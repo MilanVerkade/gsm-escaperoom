6b7fa105f45a7f6ccd537a97fcf85b0ccd10a085
SemVreugdenhil <9028980@student.zadkine.nl>
2026-03-10 12:51:14 +0100

    ...

--- js/video.js
+++ js/video.js
@@ -2,6 +2,9 @@ homepage = document.getElementById("container");
 videoContainer = document.getElementById("video-container");
 video = document.getElementById("video");
 videoContainer.style.display = "none";
+start = document.getElementById("start-button");
+
+start.addEventListener("click", openVideo);
 
 function openVideo() {
     homepage.style.display = "none";
