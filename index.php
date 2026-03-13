ca301c4aa167fd0834d651cd010c08a36398df46
MilanVerkade <9028442@student.zadkine.nl>
2026-03-13 09:35:30 +0100

    font

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Escape Room</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Alfa+Slab+One&display=swap" rel="stylesheet">
+  <title>THE LAST CHIME</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
   
 </head>
@@ -16,7 +16,7 @@
 <?php include 'nav.php'; ?>
 <div class="container" id="container">
 <img src="Foto's/12.jpg" alt="The Last Chime">
-<h1>
+<h1 id="thelastlogo">
 THE <br>
 LAST <br>
 CHIME
