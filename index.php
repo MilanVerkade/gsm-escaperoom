53c88932cfe0fdb84a260b830b68122252f54a6f
MilanVerkade <9028442@student.zadkine.nl>
2026-03-13 10:18:17 +0100

    css

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/homepage.css">
   
 </head>
 
 <body>
+<div id="fbody">
 <?php include 'nav.php'; ?>
 <div class="container" id="container">
 <img src="Foto's/12.jpg" alt="The Last Chime">
@@ -29,6 +30,7 @@ CHIME
     <video id="video" src="Video/Person_walking_into_door_d225677bb9.mp4"></video>
 </div>
 <script src="js/video.js"></script>
+</div>
 </body>
 
 </html>
