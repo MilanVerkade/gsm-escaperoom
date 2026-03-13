9208d6f34e77ae06a1088b6bee0310184db76439
SemVreugdenhil <9028980@student.zadkine.nl>
2026-03-13 10:17:36 +0100

    ...

---
+++ win-scherm.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Win scherm</title>
+</head>
+<body>
+<div class="winFoto">
+<img src="Foto's/win.png" alt="">
+</div>
+</body>
+</html>
