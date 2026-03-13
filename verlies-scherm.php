e5b41cae498d24b95729cc32955829fb55165046
SemVreugdenhil <9028980@student.zadkine.nl>
2026-03-13 11:25:20 +0100

    ...

--- verlies-scherm.php
+++ verlies-scherm.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <div class="verliesFoto">
-    <img src="Foto's/verlies.png" alt="Verlies scherm">
+    <img src="Foto's/rood.png" alt="Verlies scherm">
+    <img src="Foto's/verlies-foto.png" alt="">
 </div>
 </body>
 </html> 
--- win-scherm.php
+++ win-scherm.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Win scherm</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <div class="winFoto">
-<img src="Foto's/win.png" alt="">
+<img src="Foto's/groen.png" alt="">
+<img src="Foto's/win-foto.png" alt="">
 </div>
 </body>
 </html>
